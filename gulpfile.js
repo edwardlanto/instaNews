28301f888651aed54a38182199fef62460d8646e
Edward <edwardlanto@hotmail.com>
2017-01-25 16:13:45 -0800

    partialed files and addd flex start

--- gulpfile.js
+++ gulpfile.js
@@ -4,13 +4,16 @@ var gulp = require('gulp'),
     browserSync = require('browser-sync'),
     eslint = require('gulp-eslint'),
     plumber = require('gulp-plumber'),
-    notify = require('gulp-notify');
+    notify = require('gulp-notify'),
+    sass = require('gulp-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    cssnano = require('gulp-cssnano');
 
 
 var plumberErrorHandler = {
     errorHandler: notify.onError({
-        title: 'Gulp',
-        message: 'THIS IS AN ERROR: <%= error.message %>'
+    title: 'Gulp',
+    message: 'THIS IS AN ERROR: <%= error.message %>'
     })
 };
 
@@ -21,7 +24,19 @@ gulp.task('eslint', function () {
         .pipe(eslint())
         .pipe(eslint.format())
         .pipe(eslint.failAfterError());
-})
+});
+
+gulp.task('sass', function() {
+   gulp.src('./sass/style.scss')
+      .pipe(sass())
+      .pipe(autoprefixer({
+         browsers: ['last 2 versions']
+      }))
+      .pipe(gulp.dest('./build/css'))
+      .pipe(cssnano())
+      .pipe(rename('style.min.css'))
+      .pipe(gulp.dest('./build/css'));
+});
 
 
 
@@ -41,13 +56,14 @@ gulp.task('browser-sync', function () {
         }
     });
 
-    gulp.watch(['style.css', 'build/js/*.js']).on('change', browserSync.reload);
+    gulp.watch(['build/css/*.css', 'build/js/*.js']).on('change', browserSync.reload);
 });
 
 
 
 gulp.task('watch', function () {
-    gulp.watch('js/*.js', ['scripts']);
+gulp.watch('js/*.js', ['scripts']);
+gulp.watch('sass/*.scss', ['sass']);
 });
 
 
