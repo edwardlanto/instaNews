78c2a83554869d277334d66ae57d8a14ee43b0ec
Edward <edwardlanto@hotmail.com>
2017-01-26 23:49:15 -0800

    added animation desktopand js functions

--- js/main.js
+++ js/main.js
@@ -1,5 +1,28 @@
 $(document).ready(function(){
-
-    var message = "goodbye world!";
-    console.log(message);
+    $('.select').on('change', function(){
+        var userInput = $('.select').val();
+        var $articleList = $('.articleList')
+var url = 'https://api.nytimes.com/svc/topstories/v2/' + userInput + '.json';
+url += '?' + $.param({
+  'api-key': 'c70afdcb975e42eea907298ce2cd5830'
 });
+$.ajax({
+  urlkey: url,
+  method: 'GET',
+}).done(function(data) {
+  $.each(data.results, function(key, value){
+    var articleName = value.abstract
+    var articlePicture = value.multimedia[4].url
+    var fullArticle = '<li>' + articlePicture + articleName + '</li>'
+    console.log(data)
+    $articleList.append(articlePicture)
+  })
+
+   });
+
+  // .fail(function(err) {ç
+  // throw err;
+});
+
+    });
+// });
