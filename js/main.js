843cf92a7eff28f62e6ee5b378c6481a37d24a72
Edward <you@email.com>
2017-02-14 12:59:59 -0800

    deleted empty media query calls and fixed padding for footer

--- js/main.js
+++ js/main.js
@@ -1,60 +1,56 @@
-$(document).ready(function(){
- var $articlelist = $('.articlelist')
- var $container = $('.container')
- var $nytimage = $('.nytimage')
- var $preloader = $('.preloader')
- var errortext = 'Sorry, there was an error, please try again'
- var $navwrapper = $('.navwrapper')
+$(document).ready(function () {
+  var $articlelist = $('.articlelist')
+  var $container = $('.container')
+  var $nytimage = $('.nytimage')
+  var $preloader = $('.preloader')
+  var errortext = 'Sorry, there was an error, please try again'
+  var $navwrapper = $('.navwrapper')
 
- 
+
   $preloader.hide();
 
-$('.selector').on('change', function(){
-      var userInput = $('.selector').val();
-      $container.addClass('headershiftwitharticles')
-      $nytimage.addClass('shrinkimage')
-      $preloader.show();
-      $navwrapper.addClass('navarticleload')
-      
-      
-      
-      
+  $('.selector').on('change', function () {
+    var userInput = $('.selector').val();
+    $container.addClass('headershiftwitharticles')
+    $nytimage.addClass('shrinkimage')
+    $preloader.show();
+    $navwrapper.addClass('navarticleload')
 
-      var url = 'https://api.nytimes.com/svc/topstories/v2/' + userInput + '.json';
-      url += '?' + $.param({
+    var url = 'https://api.nytimes.com/svc/topstories/v2/' + userInput + '.json';
+    url += '?' + $.param({
       'api-key': 'c70afdcb975e42eea907298ce2cd5830'
-      });//end of param method
-$articlelist.empty();
-$.ajax({
-    url: url,
-    method: 'GET',
-})//end of ajex method
-  
-.done(function(data) {
-    $preloader.hide();
-   var $data = data.results.filter(function(item){
-  return item.multimedia.length; 
-   })// end of function call back function
-   
-  .splice(0, 12);
-    $.each($data , function(item, value){
-       var articleUrl = value.url
-       var articlePictures = ''
-       articlePictures =  '<img class ="contentcontainer"' + 'src="' + value.multimedia[4].url +'"  />'
-       var articleName = value.abstract;
-       var fullArticle = '<li>' + '<a href =' + articleUrl + '>'   + '<p class = "contenttext">' + articleName + '</p>' + articlePictures  +  '</a>' + '</li>'
-   
-      
-       
-      $articlelist.append(fullArticle);
-    });//end of each
-    // debugger;
+    });//end of param method
+    $articlelist.empty();
+    $.ajax({
+      url: url,
+      method: 'GET',
+    })//end of ajax method
 
-    })//end of done function
-.fail(function() {
-  $preloader.hide();
-  $articlelist.append(errortext);
+      .done(function (data) {
+        $preloader.hide();
+        var $data = data.results.filter(function (item) {
+          return item.multimedia.length;
+        })// end of function call back function
 
-    });//beginning of fail function 
+          .splice(0, 12);
+        $.each($data, function (item, value) {
+          var articleUrl = value.url
+          var articlePictures = ''
+          articlePictures = '<img class ="contentcontainer"' + 'src="' + value.multimedia[4].url + '"  />'
+          var articleName = value.abstract;
+          var fullArticle = '<li>' + '<a href =' + articleUrl + '>' + '<p class = "contenttext">' + articleName + '</p>' + articlePictures + '</a>' + '</li>'
+
+
+
+          $articlelist.append(fullArticle);
+        });//end of each
+        // debugger;
+
+      })//end of done function
+      .fail(function () {
+        $preloader.hide();
+        $articlelist.append(errortext);
+
+      });//beginning of fail function 
   });//onclick close
 });//document dot rdy
