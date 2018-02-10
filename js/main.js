a204f369a68bd7174d9d8e3f263dfecbe4aaa94c
edwardlanto <edwardlanto@hotmail.com>
2018-02-09 22:11:24 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,61 +1,61 @@
-$(document).ready(function () {
-  var $articlelist = $('.article-list')
-  var $container = $('.container')
-  var $nytimage = $('.nyt-image')
-  var $preloader = $('.preloader')
-  var errortext = 'Sorry, there was an error, please try again'
-  var $navwrapper = $('.nav-wrapper')
+$(document).ready(function() {
+    var $articlelist = $('.article-list')
+    var $container = $('.container')
+    var $nytimage = $('.nyt-image')
+    var $preloader = $('.preloader')
+    var errortext = 'Sorry, there was an error, please try again'
+    var $navwrapper = $('.nav-wrapper')
 
-  $('select').selectric();
+    $('select').selectric();
 
-  $preloader.hide();
+    $preloader.hide();
 
-  $('.selector').on('change', function () {
-    var userInput = $('.selector').val();
-    $container.addClass('header-shift-with-articles')
-    $nytimage.addClass('shrink-image')
-    $preloader.show();
-    $navwrapper.addClass('nav-article-load')
+    $('.selector').on('change', function() {
+        var userInput = $('.selector').val();
+        $container.addClass('header-shift-with-articles')
+        $nytimage.addClass('shrink-image')
+        $preloader.show();
+        $navwrapper.addClass('nav-article-load')
 
-    var url = 'https://api.nytimes.com/svc/topstories/v2/' + userInput + '.json';
-    url += '?' + $.param({
-      'api-key': 'c70afdcb975e42eea907298ce2cd5830'
-    });//end of param method
-    $articlelist.empty();
-    $.ajax({
-      url: url,
-      method: 'GET',
-    })//end of ajax method
+        var url = 'https://api.nytimes.com/svc/topstories/v2/' + userInput + '.json';
+        url += '?' + $.param({
+            'api-key': 'c70afdcb975e42eea907298ce2cd5830'
+        }); //end of param method
+        $articlelist.empty();
+        $.ajax({
+                url: url,
+                method: 'GET',
+            }) //end of ajax method
 
-      .always(function () {
-        $preloader.hide();
-      })
+            .always(function() {
+                $preloader.hide();
+            })
 
-      .done(function (data) {
-        var $data = data.results.filter(function (item) {
-          return item.multimedia.length;
-        }).splice(0, 12);// end of function call back function
-
-
-        $.each($data, function (item, value) {
-          var fullArticle = ''
-          fullArticle += '<li class = "article-item">' + '<a href='
-          fullArticle += value.url + '>'
-          fullArticle += '<div class ="text-container">' + '<p class = "content-text">'
-          fullArticle += value.abstract
-          fullArticle += '</p>'
-          fullArticle += '<img class = "content-container"'
-          fullArticle += 'src="' + value.multimedia[4].url + '" />'
-          fullArticle += '</a>' + '</li>'
+            .done(function(data) {
+                var $data = data.results.filter(function(item) {
+                    return item.multimedia.length;
+                }).splice(0, 12); // end of function call back function
 
 
-          $articlelist.append(fullArticle);
-        });//end of each
+                $.each($data, function(item, value) {
+                    var fullArticle = ''
+                    fullArticle += '<li class = "article-item">' + '<a href='
+                    fullArticle += value.url + '>'
+                    fullArticle += '<div class ="text-container">' + '<p class = "content-text">'
+                    fullArticle += value.abstract
+                    fullArticle += '</p>'
+                    fullArticle += '<img class = "content-container"'
+                    fullArticle += 'src="' + value.multimedia[4].url + '" />'
+                    fullArticle += '</a>' + '</li>'
 
-      })//end of done function
-      .fail(function () {
-        $articlelist.append(errortext);
 
-      });//beginning of fail function 
-  });//onclick close
-});//document dot rdy
+                    $articlelist.append(fullArticle);
+                }); //end of each
+
+            }) //end of done function
+            .fail(function() {
+                $articlelist.append(errortext);
+
+            }); //beginning of fail function 
+    }); //onclick close
+}); //document dot rdy
